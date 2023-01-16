37f2d1ebab0d8f76a047c069f27ca2fe8607e73c
RyderLu <chirst0625@gmail.com>
2023-01-16 10:18:45 +0800

    updatEproductS.py

---
+++ products.py
@@ -0,0 +1,12 @@
+#記帳程式專案
+products = []
+while True :
+	name = input('請輸入商品名稱(結束按q): ')
+	if name == 'q' :
+		break
+	price = input('請輸入商品價格: ')
+	p = [name, price]
+	products.append(p)
+print(products)
+print(products[0])  #存取二維
+print(products[0][1])
